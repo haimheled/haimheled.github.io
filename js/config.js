9dc4689e3bba1a7d2a1fb738e304bf284e3986ca
haimheled <haimheled@gmail.com>
2023-01-10 16:09:31 +0200

    Update

--- js/config.js
+++ js/config.js
@@ -87,32 +87,82 @@ const CONFIG = {
     {category: 'General', name: 'Gmail',     url: 'https://www.gmail.com'},
     {category: 'General', name: 'YouTube',   url: 'https://www.youtube.com'},
     {category: 'General', name: 'Whatsapp',  url: 'https://web.whatsapp.com'},
-
-
-    
-    {category: 'Bills', name: '012 Mobile',    url: 'https://www.012mobile.co.il/n/login'},
-    {category: 'Bills', name: '019 Mobile',    url: 'https://019mobile.co.il/userarea/'},
-    {category: 'Bills', name: 'ISP & Landline',    url: 'https://bmy.bezeq.co.il/'},
-//     {category: 'Bills', name: 'ISP & Phone', url: 'https://www.hot.net.il/heb/SelfService/Login'},
-//     {category: 'Bills', name: 'ISP content', url: 'https://www.hotnet.net.il'},
-    {category: 'Bills', name: 'Credit Card', url: 'https://www.cal-online.co.il'},
-    {category: 'Bills', name: 'Super Gas',   url: 'http://sgas.co.il'},
-    {category: 'Bills', name: 'Water',       url: 'https://www.mast.co.il/101/mail-invoice'},
-    {category: 'Bills', name: 'City Tax',    url: 'https://www.ramat-gan.muni.il/waterbillsubscribe'},
-    {category: 'Bills', name: 'Electricity', url: 'https://www.iec.co.il'},
-
-
-    {category: 'Utilities', name: 'Babylon',        url: 'http://online.babylon.com/trans_box/myindex.php?lang=EN&affiliate=CD1'},
-    {category: 'Utilities', name: 'Quick Email',    url: 'https://nospam.today'},
-    {category: 'Utilities', name: 'IP Location',    url: 'http://www.ipligence.com/geolocation'},
-    {category: 'Utilities', name: 'novnc',          url: 'http://ivory:6080/vnc.html?host=ivory&port=6080'},
     
     
+    {category: 'TV & Radio', name: 'Israel Split TV', url: 'http://www.split-tv.co.il/p/il.html'},
+    {category: 'TV & Radio', name: 'Kan 11',          url: 'http://www.kan.org.il/live/tv.aspx?stationId=2'},
+    {category: 'TV & Radio', name: 'Keshet 12',       url: 'https://www.mako.co.il/mako-vod-live-tv/VOD-6540b8dcb64fd31006.htm'},
+    {category: 'TV & Radio', name: 'Reshet 13',       url: 'http://reshet.tv/live/'},
+    {category: 'TV & Radio', name: 'US TV',           url: 'https://ustv247.tv/'},
+    {category: 'TV & Radio', name: 'Kan B',     url: 'http://rr-d.vidnt.com/player/?stream=radio-betM&account=iba&width=320&height=240&delivery_type=hls&autoplay=true&smil_profile=default'},
+    {category: 'TV & Radio', name: 'Galaz',     url: 'http://glzwizzlv.bynetcdn.com/glz_mp3?awCollectionId=misc&awEpisodeId=glz'},
+    {category: 'TV & Radio', name: 'Gal Galaz', url: 'http://glzwizzlv.bynetcdn.com/glglz_mp3?awCollectionId=misc&awEpisodeId=glglz'},
+    {category: 'TV & Radio', name: 'TLV Radio', url: 'http://www.fm1.co.il/102fm.html'},
+    {category: 'TV & Radio', name: '88FM',      url: 'http://rr-d.vidnt.com/player/?stream=radio-88fmM&account=iba&width=320&height=240&delivery_type=hls&autoplay=true&smil_profile=default'},
+
+
+    {category: 'News', name: 'Rotter',       url: 'http://rotter.net/scoopscache.html'},
+    {category: 'News', name: 'Ynet',         url: 'http://www.ynet.co.il'},
+    {category: 'News', name: 'Walla',        url: 'http://www.walla.co.il'},
+    {category: 'News', name: 'Mako',         url: 'http://www.mako.co.il'},
+//     {category: 'News', name: 'NRG',          url: 'http://www.nrg.co.il'},
+    {category: 'News', name: 'Haaretz',      url: 'http://www.haaretz.co.il'},
+    {category: 'News', name: 'News Israel',  url: 'http://www.news-israel.net'},
+    {category: 'News', name: 'Israel Today', url: 'http://www.israelhayom.co.il'},
+    {category: 'News', name: 'Google News',  url: 'https://news.google.co.il'},
+    {category: 'News', name: 'CNN',          url: 'http://www.cnn.com'},
+    {category: 'News', name: 'Fox',          url: 'http://www.foxnews.com'},
+    {category: 'News', name: 'Tikun Olam',   url: 'https://www.richardsilverstein.com/'},
+ 
+	       
+    {category: 'Sport', name: 'Sport5',          url: 'http://www.sport5.co.il'},
+    {category: 'Sport', name: 'One',             url: 'http://www.one.co.il'},
+    {category: 'Sport', name: 'NBA Games',       url: 'https://www.nba.com/games'},
+    {category: 'Sport', name: 'Wizards Streams', url: 'https://topstreams.info/nba/wizards'},
+    {category: 'Sport', name: 'NBA Streams',     url: 'https://nbabite.com'},
+    {category: 'Sport', name: 'NBA Replay',      url: 'https://nbafullhd.com'},
+
+	       
+    {category: 'Business', name: 'Globes',     url: 'http://www.globes.co.il'},
+    {category: 'Business', name: 'The Marker', url: 'http://www.themarker.co.il'},
+    {category: 'Business', name: 'Calcalist',  url: 'http://www.calcalist.co.il'},
+    
+    {category: 'Finance', name: 'Mizrahi Bank',   url: 'https://www.mizrahi-tefahot.co.il/login/bv5x83ut/index.html#/auth-page-he'},
+    {category: 'Finance', name: 'Discount Bank',  url: 'https://start.telebank.co.il/login/#/LOGIN_PAGE'},
+    {category: 'Finance', name: 'Exchange Rates', url: 'http://www.boi.org.il/he/Markets/ExchangeRates/Pages/Default.aspx'},
+    {category: 'Finance', name: 'Visa Cal',           url: 'https://services.cal-online.co.il/Card-Holders/SCREENS/AccountManagement/Login.aspx?ReturnUrl=%2fcard-holders%2fScreens%2fAccountManagement%2fHomePage.aspx'},
+    
+
     {category: 'Shopping', name: 'Israel Post',url: 'https://israelpost.co.il/%d7%a9%d7%99%d7%a8%d7%95%d7%aa%d7%99%d7%9d/%d7%94%d7%97%d7%91%d7%99%d7%9c%d7%95%d7%aa-%d7%a9%d7%9c%d7%99/'},
     {category: 'Shopping', name: 'Reut Buy',   url: 'https://reutbuyitforme.com'},
     {category: 'Shopping', name: 'Ali Buy',    url: 'https://ali-buy.com'},
     {category: 'Shopping', name: 'I Need It',  url: 'https://ineedit.co.il'},
-    
+
+
+    {category: 'Health', name: 'Clalit', url: 'https://e-services.clalit.org.il/onlineweb/general/login.aspx'},
+
+
+    {category: 'Utilities', name: 'Babylon',       url: 'http://online.babylon.com/trans_box/myindex.php?lang=EN&affiliate=CD1'},
+    {category: 'Utilities', name: 'Quick Email',   url: 'https://nospam.today'},
+    {category: 'Utilities', name: 'Wikipedia',     url: 'https://he.wikipedia.org'},
+    {category: 'Utilities', name: 'IP Address',    url: 'https://ipecho.net/plain'},
+    {category: 'Utilities', name: 'IP Location',   url: 'https://iplocation.com/'},
+    {category: 'Utilities', name: 'Speed Test',    url: 'http://www.speedtest.net'},
+    {category: 'Utilities', name: 'Google',        url: 'https://www.google.com'},
+    {category: 'Utilities', name: 'Duckduckgo',    url: 'https://duckduckgo.com/'},
+    {category: 'Utilities', name: 'Bing',          url: 'https://www.bing.com/'},
+    {category: 'Utilities', name: 'ChatGPT',       url: 'https://chat.openai.com/auth/login'},
+    {category: 'Utilities', name: 'Remote Access', url: 'https://150.co.il'},
+    {category: 'Utilities', name: 'PC Builder',    url: 'https://pcpartpicker.com'},
+//     {category: 'Utilities', name: 'novnc',          url: 'http://ivory:6080/vnc.html?host=ivory&port=6080'},
+//     {category: 'Utilities', name: 'IP Address',    url: 'ifconfig.me'},
+//     {category: 'Utilities', name: 'IP Location',   url: 'https://www.iplocation.net/'},
+
+
+    {category: 'Development', name: 'Github Page', url: 'https://github.com/haimheled/haimheled.github.io'},
+    {category: 'Development', name: 'Github Edit', url: 'https://github.com/haimheled/haimheled.github.io/edit/master/js/config.js'},
+    {category: 'Development', name: 'Github Edit Old', url: 'https://github.com/haimheled/haimheled.github.io/blob/master/startpage.rwrt.old_site/index.html'},
+
 
     {category: 'Renesas', name: 'Portal',        url: 'https://urldefense.proofpoint.com/v2/url?u=https-3A__renesasgroup.sharepoint.com_sites_GIPen&d=DwMFAg&c=euGZstcaTDllvimEN8b7jXrwqOf-v5A_CdpgnVfiiMM&r=UwQdxjYfFiaTT2Adbb4SkV5eaLla8cQDERLmGpwUsXM&m=sjsiFNIloFkDZSS7YbIl_lvjnK7-laA3IhuC8HqK3Eg&s=4Kqhc4V8ADEctMewZo2ovz_tkInA2Hj3KO5aRWPhaQk&e='},
     {category: 'Renesas', name: 'Mail',          url: 'https://outlook.office365.com/mail/inbox'},
@@ -147,78 +197,16 @@ const CONFIG = {
     {category: 'Renesas', name: 'Soc CL3000',    url: 'http://10.220.220.201/soc-cl3000/soconline/app/'},
 
 
-    {category: 'Development', name: 'Github Page', url: 'https://github.com/haimheled/haimheled.github.io'},
-    {category: 'Development', name: 'Github Edit', url: 'https://github.com/haimheled/haimheled.github.io/edit/master/js/config.js'},
-    {category: 'Development', name: 'Github Edit Old', url: 'https://github.com/haimheled/haimheled.github.io/blob/master/startpage.rwrt.old_site/index.html'},
-
-
-    {category: 'News', name: 'Rotter',       url: 'http://rotter.net/scoopscache.html'},
-    {category: 'News', name: 'Ynet',         url: 'http://www.ynet.co.il'},
-    {category: 'News', name: 'Walla',        url: 'http://www.walla.co.il'},
-    {category: 'News', name: 'Mako',         url: 'http://www.mako.co.il'},
-//     {category: 'News', name: 'NRG',          url: 'http://www.nrg.co.il'},
-    {category: 'News', name: 'Haaretz',      url: 'http://www.haaretz.co.il'},
-    {category: 'News', name: 'News Israel',  url: 'http://www.news-israel.net'},
-    {category: 'News', name: 'Israel Today', url: 'http://www.israelhayom.co.il'},
-    {category: 'News', name: 'Google News',  url: 'https://news.google.co.il'},
-    {category: 'News', name: 'CNN',          url: 'http://www.cnn.com'},
-    {category: 'News', name: 'Fox',          url: 'http://www.foxnews.com'},
-    {category: 'News', name: 'Tikun Olam',   url: 'https://www.richardsilverstein.com/'},
- 
-	       
-    {category: 'Sport', name: 'Sport5',          url: 'http://www.sport5.co.il'},
-    {category: 'Sport', name: 'One',             url: 'http://www.one.co.il'},
-    {category: 'Sport', name: 'NBA Games',       url: 'https://www.nba.com/games'},
-    {category: 'Sport', name: 'Wizards Streams', url: 'https://topstreams.info/nba/wizards'},
-    {category: 'Sport', name: 'NBA Streams',     url: 'https://nbabite.com'},
-    {category: 'Sport', name: 'NBA Replay',      url: 'https://nbafullhd.com'},
-
-	       
-    {category: 'Business', name: 'Globes',     url: 'http://www.globes.co.il'},
-    {category: 'Business', name: 'The Marker', url: 'http://www.themarker.co.il'},
-    {category: 'Business', name: 'Calcalist',  url: 'http://www.calcalist.co.il'},
-    
-    {category: 'Finance', name: 'Mizrahi Bank',   url: 'https://www.mizrahi-tefahot.co.il/login/bv5x83ut/index.html#/auth-page-he'},
-    {category: 'Finance', name: 'Discount Bank',  url: 'https://start.telebank.co.il/login/#/LOGIN_PAGE'},
-    {category: 'Finance', name: 'Exchange Rates', url: 'http://www.boi.org.il/he/Markets/ExchangeRates/Pages/Default.aspx'},
-    {category: 'Finance', name: 'Visa Cal',           url: 'https://services.cal-online.co.il/Card-Holders/SCREENS/AccountManagement/Login.aspx?ReturnUrl=%2fcard-holders%2fScreens%2fAccountManagement%2fHomePage.aspx'},
-
-
-    {category: 'Health', name: 'Clalit', url: 'https://e-services.clalit.org.il/onlineweb/general/login.aspx'},
-
-
-    {category: 'Tools', name: 'Dictionary',    url: 'http://online.babylon.com/trans_box/myindex.php?lang=EN&affiliate=CD1'},
-    {category: 'Tools', name: 'Wikipedia',     url: 'https://he.wikipedia.org'},
-    {category: 'Tools', name: 'IP Address',    url: 'http://whatip.me'},
-    {category: 'Tools', name: 'IP Location',   url: 'http://www.ipligence.com/geolocation'},
-    {category: 'Tools', name: 'Speed Test',    url: 'http://www.speedtest.net'},
-    {category: 'Tools', name: 'Google Search', url: 'https://www.google.co.il'},
-    {category: 'Tools', name: 'Remote Access', url: 'https://150.co.il'},
-    {category: 'Tools', name: 'PC Builder',    url: 'https://pcpartpicker.com'},
-
-
-    {category: 'TV', name: 'Israel Split TV', url: 'http://www.split-tv.co.il/p/il.html'},
-    {category: 'TV', name: 'Kan 11',          url: 'http://www.kan.org.il/live/tv.aspx?stationId=2'},
-    {category: 'TV', name: 'Keshet 12',       url: 'https://www.mako.co.il/mako-vod-live-tv/VOD-6540b8dcb64fd31006.htm'},
-    {category: 'TV', name: 'Reshet 13',       url: 'http://reshet.tv/live/'},
-    {category: 'TV', name: 'US TV',           url: 'https://ustv247.tv/'},
-    
-
-    {category: 'Radio', name: 'Kan B',     url: 'http://rr-d.vidnt.com/player/?stream=radio-betM&account=iba&width=320&height=240&delivery_type=hls&autoplay=true&smil_profile=default'},
-    {category: 'Radio', name: 'Galaz',     url: 'http://glzwizzlv.bynetcdn.com/glz_mp3?awCollectionId=misc&awEpisodeId=glz'},
-    {category: 'Radio', name: 'Gal Galaz', url: 'http://glzwizzlv.bynetcdn.com/glglz_mp3?awCollectionId=misc&awEpisodeId=glglz'},
-    {category: 'Radio', name: 'TLV Radio', url: 'http://www.fm1.co.il/102fm.html'},
-    {category: 'Radio', name: '88FM',      url: 'http://rr-d.vidnt.com/player/?stream=radio-88fmM&account=iba&width=320&height=240&delivery_type=hls&autoplay=true&smil_profile=default'},
-    
-
-
-
-
-
-
-
-
-
+    {category: 'Bills', name: '012 Mobile',    url: 'https://www.012mobile.co.il/n/login'},
+    {category: 'Bills', name: '019 Mobile',    url: 'https://019mobile.co.il/userarea/'},
+    {category: 'Bills', name: 'ISP & Landline',    url: 'https://bmy.bezeq.co.il/'},
+//     {category: 'Bills', name: 'ISP & Phone', url: 'https://www.hot.net.il/heb/SelfService/Login'},
+//     {category: 'Bills', name: 'ISP content', url: 'https://www.hotnet.net.il'},
+    {category: 'Bills', name: 'Credit Card', url: 'https://www.cal-online.co.il'},
+    {category: 'Bills', name: 'Super Gas',   url: 'http://sgas.co.il'},
+    {category: 'Bills', name: 'Water',       url: 'https://www.mast.co.il/101/mail-invoice'},
+    {category: 'Bills', name: 'City Tax',    url: 'https://www.ramat-gan.muni.il/waterbillsubscribe'},
+    {category: 'Bills', name: 'Electricity', url: 'https://www.iec.co.il'},
 
 
 
