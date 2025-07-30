7c398e6a51ee7e5cea02f59713d27a2a520f809c
haimheled <haimheled@gmail.com>
2025-07-30 22:08:07 +0300

    Update config.js

--- js/config.js
+++ js/config.js
@@ -153,7 +153,8 @@ quickLaunch: false,
     
     {category: 'Health', name: 'Clalit', url: 'https://e-services.clalit.org.il/onlineweb/general/login.aspx'},
     {category: 'Health', name: 'My Fitness Pal', url: 'https://www.myfitnesspal.com/account/login'},
-               
+    {category: 'Health', name: 'Cronometer', url: 'https://cronometer.com/login'},
+                          
     
     {category: 'Utilities', name: 'Babylon',          url: 'http://online.babylon.com/trans_box/myindex.php?lang=EN&affiliate=CD1'},
     {category: 'Utilities', name: 'Quick Email',      url: 'https://nospam.today'},
